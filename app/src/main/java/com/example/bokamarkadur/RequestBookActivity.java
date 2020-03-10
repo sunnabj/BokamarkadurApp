f850f6e05ce3f8df4cbd9d19c6710130efe6baee
Þórdís Pétursdóttir <thp44@hi.is>
2020-03-10 21:24:31 +0000

    Hægt að bæta við Request Book

--- app/src/main/java/com/example/bokamarkadur/RequestBookActivity.java
+++ app/src/main/java/com/example/bokamarkadur/RequestBookActivity.java
@@ -1,8 +1,14 @@
 package com.example.bokamarkadur;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.telephony.mbms.FileInfo;
@@ -10,27 +16,35 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.bokamarkadur.POJO.Book;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.gson.JsonObject;
 
+import java.io.File;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RequestBookActivity extends AppCompatActivity {
 
-    /**
-     * Virkni hér er ekki tilbúin, kóðinn hér er mest megnis prufukóði.
-     */
-
+    private Button uploadImage;
     private Button submit;
+    private Uri selectedImage;
+    private String imgDecodableString;
+    private ImageView viewUploadedImage;
     private ProgressDialog progressDialog;
+    private static final int GALLERY_REQUEST_CODE = 1999;
 
     APIInterface apiInterface;
 
@@ -38,10 +52,19 @@ public class RequestBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_book);
+        uploadImage = (Button) findViewById(R.id.btnUploadImage);
         submit = (Button) findViewById(R.id.submit);
+        viewUploadedImage = (ImageView) findViewById(R.id.uploadImage);
 
         // Tengjumst API Interface sem talar við bakendann okkar.
         apiInterface = APIClient.getClient().create(APIInterface.class);
+
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickFromGallery();
+            }
+        });
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,63 +72,100 @@ public class RequestBookActivity extends AppCompatActivity {
                 submitData();
             }
         });
-
-        /**+
-         *  Bottom navigation.
-         */
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.dashboard:
-                        startActivity(new Intent(getApplicationContext(),
-                                AllBooksActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(),
-                                MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.about:
-                        startActivity(new Intent(getApplicationContext(),
-                                AboutusActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
-            }
-        });
     }
 
-    private void submitData(){
+    private void pickFromGallery() {
+        try {
+            if (ActivityCompat.checkSelfPermission(RequestBookActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(RequestBookActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, GALLERY_REQUEST_CODE);
+            } else {
+                //Create an Intent with action as ACTION_PICK
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                // Sets the type as image/*. This ensures only components of type image are selected
+                intent.setType("image/*");
+                //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+                String[] mimeTypes = {"image/jpeg", "image/png"};
+                intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+                // Launching the Intent
+                startActivityForResult(intent, GALLERY_REQUEST_CODE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    // Get user permission to access gallery.
+                    if (ContextCompat.checkSelfPermission(RequestBookActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        // Permission is not granted
+                    } else {
+                        //data.getData returns the content URI for the selected Image
+                        selectedImage = data.getData();
+
+                        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                        // Get the cursor
+                        Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                        // Move to first row
+                        cursor.moveToFirst();
+                        //Get the column index of MediaStore.Images.Media.DATA
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        //Gets the String value in the column
+                        imgDecodableString = cursor.getString(columnIndex);
+                        cursor.close();
+
+                        viewUploadedImage.setImageURI(selectedImage);
+                    }
+                    break;
+            }
+    }
+
+    private void submitData() {
 
         EditText title = (EditText) findViewById(R.id.edtTitle);
         EditText author = (EditText) findViewById(R.id.edtAuthor);
         EditText edition = (EditText) findViewById(R.id.edtEdition);
-        progressDialog = new ProgressDialog(this);
+        EditText subject = (EditText) findViewById(R.id.edtSubject);
+
+        progressDialog = new ProgressDialog(RequestBookActivity.this);
         //progressDialog.setMessage(getString(R.string.loading));
         progressDialog.setCancelable(false);
         progressDialog.show();
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("title", "1");
-        jsonObject.addProperty("author", "1");
-        jsonObject.addProperty("edition", "1");
-        jsonObject.addProperty("subject", "COMPUTERSCIENCE");
-        //jsonObject.addProperty("file", "null");
+        // Initiate file here
+        File file = new File(imgDecodableString);
+        // Create a request body with file and image media type
+        okhttp3.RequestBody fileReqBody = okhttp3.RequestBody.create(okhttp3.MediaType.parse("image/*"), file);
+        // Create MultipartBody.Part using file request-body,file name and part name
+        MultipartBody.Part imagePart = MultipartBody.Part.createFormData("file", file.getName(), fileReqBody);
 
-        Call<Book> newBookRequested = apiInterface.addBookRequested(jsonObject);
-        newBookRequested.enqueue(new Callback<Book>() {
+        // Get values from form.
+        String titlePart = title.getText().toString();
+        String authorPart = author.getText().toString();
+        int editionPart = Integer.parseInt(edition.getText().toString());
+        String subjectPart = subject.getText().toString();
+
+        Call<Book> newBookForSale = apiInterface.addBookRequested("application/json", "Bearer " + LoginActivity.token,
+                imagePart, titlePart, authorPart, editionPart, subjectPart);
+        newBookForSale.enqueue(new Callback<Book>() {
             @Override
             public void onResponse(Call<Book> call, Response<Book> response) {
                 //hiding progress dialog
                 progressDialog.dismiss();
-                if(response.isSuccessful()){
-                    Toast.makeText(getApplicationContext(), "Book: "+response.body().getTitle()+" has been added", Toast.LENGTH_LONG).show();
+                Log.d("onResponse: ", String.valueOf(response.body()));
+                if (response.isSuccessful()) {
                     openMainActivity();
-                    Log.d("myTag", String.valueOf(response.body().getUser()));
+                    Log.d("Success: ", response.body().getTitle() + " has been added.");
+                } else {
+                    try {
+                        Log.d("error", response.errorBody().string());
+                    } catch (Exception e) {
+                        Log.d("error: ", e.getMessage());
+                    }
                 }
             }
 
@@ -113,12 +173,13 @@ public class RequestBookActivity extends AppCompatActivity {
             public void onFailure(Call<Book> call, Throwable t) {
                 progressDialog.dismiss();
                 Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+                Log.d("myTag", "HELPHLEP");
             }
         });
     }
 
     public void openMainActivity() {
-        Intent intent= new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
